dc5d50b5f0dbcb4802a82090356b4b7dcba7f2a5
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-25 22:15:16 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const timerText = document.getElementById('timerText');
+const btnStart = document.getElementById('btnStart');
+const btnPause = document.getElementById('btnPause');
+let count = 0;
+let intervalID;
+
+btnStart.addEventListener('click', timer);
+function timer(){
+    intervalID = setInterval(() => {
+        count += 1;
+        timerText.innerHTML = count;
+    }, 1000);
+};
